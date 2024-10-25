dbb3a58b1be28e602db226471ece50000ddf5252
jm1021 <jmort1021@gmail.com>
2024-10-25 08:40:19 -0700

    extreme comments

--- model/post.py
+++ model/post.py
@@ -27,15 +27,40 @@ class Post(db.Model):
     _group_id = db.Column(db.Integer, db.ForeignKey('groups.id'), nullable=False)
 
     def __init__(self, title, content, user_id, group_id):
+        """
+        Constructor, 1st step in object creation.
+        
+        Args:
+            title (str): The title of the post.
+            content (str): The content of the post.
+            user_id (int): The user who created the post.
+            group_id (int): The group to which the post belongs.
+        """
         self._title = title
         self._content = content
         self._user_id = user_id
         self._group_id = group_id
 
     def __repr__(self):
+        """
+        The __repr__ method is a special method used to represent the object in a string format.
+        Called by the repr(post) built-in function, where post is an instance of the Post class.
+        
+        Returns:
+            str: A text representation of how to create the object.
+        """
         return f"Post(id={self.id}, title={self._title}, content={self._content}, user_id={self._user_id}, group_id={self._group_id})"
 
     def create(self):
+        """
+        The create method adds the object to the database and commits the transaction.
+        
+        Uses:
+            The db ORM methods to add and commit the transaction.
+        
+        Raises:
+            Exception: An error occurred when adding the object to the database.
+        """
         try:
             db.session.add(self)
             db.session.commit()
@@ -43,9 +68,16 @@ class Post(db.Model):
             db.session.rollback()
             raise e
         
-    # CRUD read converts self to dictionary
-    # returns dictionary
     def read(self):
+        """
+        The read method retrieves the object data from the object's attributes and returns it as a dictionary.
+        
+        Uses:
+            The Group.query and User.query methods to retrieve the group and user objects.
+        
+        Returns:
+            dict: A dictionary containing the post data, including user and group names.
+        """
         user = User.query.get(self._user_id)
         group = Group.query.get(self._group_id)
         data = {
@@ -58,21 +90,51 @@ class Post(db.Model):
         return data
     
     def update(self):
+        """
+        The update method commits the transaction to the database.
+        
+        Uses:
+            The db ORM method to commit the transaction.
+        
+        Raises:
+            Exception: An error occurred when updating the object in the database.
+        """
         try:
             db.session.commit()
         except Exception as e:
             db.session.rollback()
             raise e
-        
+    
     def delete(self):
+        """
+        The delete method removes the object from the database and commits the transaction.
+        
+        Uses:
+            The db ORM methods to delete and commit the transaction.
+        
+        Raises:
+            Exception: An error occurred when deleting the object from the database.
+        """    
         try:
             db.session.delete(self)
             db.session.commit()
         except Exception as e:
             db.session.rollback()
             raise e
-        
+
 def initPosts():
+    """
+    The initPosts function creates the Post table and adds tester data to the table.
+    
+    Uses:
+        The db ORM methods to create the table.
+    
+    Instantiates:
+        Post objects with tester data.
+    
+    Raises:
+        IntegrityError: An error occurred when adding the tester data to the table.
+    """        
     with app.app_context():
         """Create database and tables"""
         db.create_all()
@@ -86,6 +148,7 @@ def initPosts():
         for post in [p1, p2, p3, p4]:
             try:
                 post.create()
+                print(f"Record created: {repr(post)}")
             except IntegrityError:
                 '''fails with bad or duplicate data'''
                 db.session.remove()
